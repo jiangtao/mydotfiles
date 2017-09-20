6c3be5c2b4fb0be94ee6704b452acfa58a4a457a
jiangtao <jiangtao@users.noreply.github.com>
2017-09-20 02:33:53 -0500

    default use oh-my-zsh

--- boostrap.sh
+++ boostrap.sh
@@ -62,6 +62,10 @@ apps=(
 echo "installing apps..."
 brew cask install --appdir="/Applications" ${apps[@]}
 
+# oh my zsh 
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # create usual dirs
 
 echo "making dirs..."
